3b61e4948e037c0fc54c8ccb381ae1a0195ddc9e
Myrkvithr <place_of_meeting@inbox.ru>
2020-01-08 15:11:17 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+﻿var mixer = mixitup('.container-filter');
+
+$(document).ready(function () {
+
+    /* Меню плавная прокрутка*/
+    $('a[href^="#"').click(function(){
+        var target = $(this).attr('href'); //Сохраняем значение атрибута href в переменной:
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+    });
+
+/* Кнопка наверх */
+$(window).scroll(function() {
+    if ($(this).scrollTop() != 0)
+        $('#toTop').fadeIn();
+    else
+        $('#toTop').fadeOut();
+});
+$('#toTop').click(function() {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 800);
+});
+});
